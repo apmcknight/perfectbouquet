2fd0383103ae28bc8f2dc02669d2edf15f1920af
Adam McKnight <adammcknight@me.com>
2018-11-05 11:12:53 -0500

    Added page transitions, created menu page, add menu elements.

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $(".animsition").animsition({
     inClass: 'fade-in-up',
     outClass: 'fade-out-up',
@@ -13,14 +13,12 @@ $(document).ready(function () {
     timeout: false,
     timeoutCountdown: 5000,
     onLoadEvent: true,
-    browser: ['animation-duration', '-webkit-animation-duration'],
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay: false,
-    overlayClass: 'animsition-overlay-slide',
-    overlayParentElement: 'body',
-    transition: function (url) {
-      window.location.href = url;
-    }
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
   });
 });
