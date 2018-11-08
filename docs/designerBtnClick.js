6d8db2c4269adaa58d216b92066a0957ecb281c2
Adam McKnight <adammcknight@me.com>
2018-11-08 13:29:23 -0500

    Finished Designer page step 1, started JavaScript for button selectors.

---
+++ docs/designerBtnClick.js
@@ -0,0 +1,8 @@
+// Need to figure out how to change class name on click, so a 
+// selection can be made between "small or large" 
+
+function changeSelector(){
+    let sizeSlector = document.getElementsByClassName('sizeBtn').addEventListener('click', sizeBtnClicked);
+}
+
+console.log(changeSelector);
