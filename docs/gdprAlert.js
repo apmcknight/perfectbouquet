1b125c9b30b91cf646a641e1b16489086e3b92b5
Adam McKnight <adammcknight@me.com>
2018-11-18 17:33:23 -0500

    Added JavaScript to close the GDPR Alert Window. Edited the Unsupported font-family and size. Added a mediaquery to account for Unsupported showing on larger screens.

--- docs/gdprAlert.js
+++ docs/gdprAlert.js
@@ -1,8 +1,12 @@
-$(document).ready(function(){   
-    setTimeout(function () {
-        $("#gdprWindow").fadeIn(200);
-     }, 4000);
-    $(".hide, .hide").click(function() {
-        $("#gdprBtn").fadeOut(200);
-    }); 
-}); 
+// Developed by Adam McKnight, in November of 2018.
+
+let exitButton = document.querySelector('#gdprAlert>#gdprButton'); //Links the GDPR Alert Button to JavaScript
+const gdprAlert = document.querySelector('#gdprWindow') //Links the GDPR Alert Window itself to JavaScript, this element does not change.
+
+// Function sets the display value of CSS to 'none', which hides the 
+// GDPR Alert Window. — Function is called/envoked in the HTML itself.
+
+function closeAlertWindow(){
+    gdprAlert.style.display = 'none';
+}
+
